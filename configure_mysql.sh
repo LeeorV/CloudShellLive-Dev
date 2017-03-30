68a9f438f411424b5e41b9e58aff1b1a9c67a669
Leeor.V <leeor.v@qualisystems.com>
2017-03-30 14:54:12 +0300

    Fixed append output of mysql commands

--- configure_mysql.sh
+++ configure_mysql.sh
@@ -11,7 +11,7 @@ echo "####	Install MySQL">>/script_output.txt
 apt-get -y install mysql-server >>/script_output.txt
 
 echo "####	Create DB">>/script_output.txt
-mysql --batch --silent -e CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+mysql --batch --silent -e CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci >>/script_output.txt
 
 echo "####	Grant permissions to Wordpress DB to new wordpress user account">>/script_output.txt
-mysql --batch --silent -e GRANT ALL ON wordpress.* TO 'wordpressuser'@'%' IDENTIFIED BY '$CS_SQL_PASSWD';
+mysql --batch --silent -e GRANT ALL ON wordpress.* TO 'wordpressuser'@'%' IDENTIFIED BY '$CS_SQL_PASSWD' >>/script_output.txt
